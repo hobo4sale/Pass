264a955854dc07c2193e7f19bf5b61f56e049e2f
Richard Ho <rho@Richards-MacBook-Pro.local>
2016-11-13 16:36:12 -0600

    restructured code

--- app/src/main/java/com/boyz/rho/pass/Activities/MainActivity.java
+++ app/src/main/java/com/boyz/rho/pass/Activities/MainActivity.java
@@ -1,4 +1,4 @@
-package com.boyz.rho.pass;
+package com.boyz.rho.pass.Activities;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -12,6 +12,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.boyz.rho.pass.R;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
